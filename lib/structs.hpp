f1a61159b322208112e0838b5b6d2a12bec9f2b6
RivasDiego <00121520@uca.edu.sv>
2022-10-23 01:48:03 -0600

    Working with fstream library :alembic:

--- lib/structs.hpp
+++ lib/structs.hpp
@@ -19,22 +19,3 @@ struct node
 };
 
 typedef struct node Node;
-
-void AddAtStart (T data, Node **list);
-void AddAtEnd (T data, Node **list);
-void AddAfter (T data, Node **list);
-void AddBefore (T data, Node **list);
-
-void RemoveAtStart (Node** list);
-void RemoveAtEnd (Node** list);
-void RemoveAfter (Node** list);
-void RemoveBefore (Node** list);
-
-void SaveToFile();
-void _init_();
-
-Word GetFirst(Node** list);
-Word GetLast(Node** list);
-Word GetWordInfo(Node** list);
-
-string DateTimeStamp ();
