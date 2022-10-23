8e3e1ddb3bf8681b60430c9efdbcbae345e90a44
RivasDiego <00121520@uca.edu.sv>
2022-10-23 02:06:04 -0600

    Updated ReadMe file :memo:

--- P01/main.cpp
+++ P01/main.cpp
@@ -1,9 +1,3 @@
-// Crear un programa que simule un diccionario que cuente como máximo con 5 palabras por cada letra
-//     del alfabeto, que le permita al usuario realizar las siguientes acciones:
-
-//     1.  Agregar palabras, significado, sinónimo, antónimo, entre otros
-//     2.  Eliminar una palabra
-//     3.  Buscar palabras en específico
 #include <iostream>
 #include <fstream>
 #include <string>
