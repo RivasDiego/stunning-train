69a656bd93f8a9cedb97cd408d5a864d89f63093
RivasDiego <00121520@uca.edu.sv>
2022-11-02 00:32:13 -0600

    Menu's display changed :speech_balloon:

--- P01/main.cpp
+++ P01/main.cpp
@@ -23,8 +23,8 @@ int main()
     do
     {
         cout << "\nMENU PRINCIPAL\n";
-        cout << "\t1 - Agregar elementos\n";
-        cout << "\t2 - Eliminar elementos\n";
+        cout << "\t1 - Agregar registros\n";
+        cout << "\t2 - Eliminar registros\n";
         cout << "\t3 - Mostrar registros actuales\n";
         cout << "\t4 - Salir\n";
         cout << "\nPor favor, ingrese una opcion: ";
@@ -65,11 +65,11 @@ void AddElements(Node **list)
     string ui_word;
     do
     {
-        cout << "\n---- Agregar elementos -----\n";
-        cout << "\t1 - Agregar al principio\n";
-        cout << "\t2 - Agregar al final\n";
-        cout << "\t3 - Agregar antes de alguna palabra\n";
-        cout << "\t4 - Agregar despues de alguna palabra\n";
+        cout << "\n---- Agregar registros -----\n";
+        cout << "\t1 - Agregar registro al principio\n";
+        cout << "\t2 - Agregar registro al final\n";
+        cout << "\t3 - Agregar registro antes de alguna palabra\n";
+        cout << "\t4 - Agregar registro despues de alguna palabra\n";
         cout << "\t5 - Salir\n";
         cout << "Por favor, ingrese una opcion: ";
         cin >> menu_option;
@@ -86,7 +86,7 @@ void AddElements(Node **list)
             cout << "La operacion se ejecuto exitosamente\n";
             break;
         case 3:
-            cout << "Ingresa la palabra: ";
+            cout << "Ingresa la palabra a buscar: ";
             cin >> ui_word;
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
             AddBefore(CreateWord(), list, ui_word);
@@ -94,7 +94,7 @@ void AddElements(Node **list)
             show_menu = true;
             break;
         case 4:
-            cout << "Ingresa la palabra: ";
+            cout << "Ingresa la palabra a buscar: ";
             cin >> ui_word;
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
             AddAfter(CreateWord(), list, ui_word);
@@ -120,10 +120,10 @@ void RemoveElements(Node **list)
     string ui_word;
     do
     {
-        cout << "\n---- Eliminar elementos -----\n";
-        cout << "\t1 - Eliminar al principio\n";
-        cout << "\t2 - Eliminar al final\n";
-        cout << "\t3 - Eliminar alguna palabra\n";
+        cout << "\n---- Eliminar registros -----\n";
+        cout << "\t1 - Eliminar registro al principio\n";
+        cout << "\t2 - Eliminar registro al final\n";
+        cout << "\t3 - Eliminar registro de alguna palabra\n";
         cout << "\t4 - Eliminar todos los registros\n";
         cout << "\t5 - Salir\n";
         cout << "Por favor, ingrese una opcion: ";
@@ -140,7 +140,7 @@ void RemoveElements(Node **list)
             show_menu = true;
             break;
         case 3:
-            cout << "Ingresa la palabra: ";
+            cout << "Ingresa la palabra a buscar: ";
             cin >> ui_word;
             RemoveWord(list, ui_word);
             ui_word = "";
@@ -167,7 +167,7 @@ void ShowElements(Node **list)
     string ui_word;
     do
     {
-        cout << "\n---- Mostrar elementos -----\n";
+        cout << "\n---- Mostrar registros -----\n";
         cout << "\t1 - Mostrar todos los registros\n";
         cout << "\t2 - Mostrar el registro de alguna palabra\n";
         cout << "\t3 - Salir\n";
@@ -180,7 +180,7 @@ void ShowElements(Node **list)
             show_menu = true;
             break;
         case 2:
-            cout << "Ingresa la palabra: ";
+            cout << "Ingresa la palabra a buscar: ";
             cin >> ui_word;
             PrintWord(ui_word, list);
             ui_word = "";
